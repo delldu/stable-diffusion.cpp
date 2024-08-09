e9e5ad96563467cc78173a6ecf1192a21301e0c7
dell du <18588220928@163>
2024-08-09 21:53:25 +0800

    Update

--- project/b.sh
+++ project/b.sh
@@ -1,52 +1,23 @@
-./build/bin/sd -v --mode img2img \
-	--model models/sdxl_turbo_q8_0.gguf \
-	--height 1024 --width 1024 \
-	--init-img "images/4guys.png" \
-	--prompt "4 guys standing in back of sea, sunset, four person face to camera !!!" \
-	--negative-prompt "black and white" \
-	--strength 0.15 \
-	--cfg-scale 1.8 \
-	--steps 5 --seed -1 \
-	-o /tmp/output.png
-
-
-
 # ./build/bin/sd -v --mode img2img \
-# 	--model models/sd21_turbo_q8_0.gguf \
+# 	--model models/sdxl_turbo_q8_0.gguf \
 # 	--height 1024 --width 1024 \
-#  	--init-img "images/4guys.png" \
-# 	--prompt "4 guys" \
+# 	--init-img "images/4guys.png" \
+# 	--prompt "4 guys standing in back of sea, sunset, four person face to camera !!!" \
 # 	--negative-prompt "black and white" \
-# 	--strength 0.20 \
-# 	--cfg-scale 7.5 \
-# 	--steps 25 \
+# 	--strength 0.15 \
+# 	--cfg-scale 1.8 \
+# 	--steps 5 --seed -1 \
 # 	-o /tmp/output.png
 
 
-# ./build/bin/sd --mode img2img \
-# 	--model models/sd21_turbo_q8_0.gguf \
-# 	--height 640 --width 512 \
-# 	--init-img images/bag.png \
-# 	--prompt 'a children bag' --negative-prompt 'black and white' \
-# 	--strength 0.80 \
-# 	--control-net models/sd21_canny_q8_0.gguf --control-image images/bag.png \
-# 	--canny --control-strength 0.70 \
-# 	--cfg-scale 7.5 --steps 25 \
-# 	-o /tmp/output.png
-
-
-# ./build/bin/sd --mode img2img \
-# 	--model models/sd21_turbo_q8_0.gguf \
-# 	--height 640 --width 512 \
-# 	--init-img images/bag.png \
-# 	--prompt 'a children bag' --negative-prompt 'black and white' \
-# 	--strength 0.10 \
-# 	--control-net models/sd21_canny_q8_0.gguf \
-# 	--control-image images/bag_scribble.png \
-# 	--control-strength 0.70 \
-# 	--cfg-scale 7.5 --steps 25 \
-# 	-o /tmp/output.png
-
+PROMPT="interior design of a luxurious master bedroom, gold and marble furniture, luxury, intricate, breathtaking"
+./build/bin/sd --mode txt2img --model models/sdxl_turbo_q8_0.gguf \
+	--height 768 --width 512 \
+	--prompt "'$PROMPT'" \
+	--negative-prompt "'$NEGATIVE'" \
+	--strength 0.90 \
+	--cfg-scale 1.8 --steps 5 \
+	-o /tmp/output.png
 
 
 display /tmp/output.png
